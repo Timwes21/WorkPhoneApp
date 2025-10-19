e2375106148dba7307a77e25d51ab3b00140bd4b
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-10-19 10:56:12 -0400

    added the location of the ceritificate for pymongo

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -2,15 +2,15 @@ from pymongo import AsyncMongoClient
 from motor.motor_asyncio import AsyncIOMotorClient
 import pymongo
 from dotenv import load_dotenv
-import os
+import os, certifi
 load_dotenv()
 
 
 def get_mongo_collections():
     url = os.environ["MONGO_URL"]
-    url = os.environ["MONGO_URL"]
-    client = AsyncIOMotorClient(url)
+    client = AsyncIOMotorClient(url, tls=True, tlsCAFile=certifi.where())
     db = client["WorkPhone"]
+
     return db["call_logs"], db["docs"], db["user_info"], client
 
 
