ad39bed38fce2638c253ddb24e48a01eb511ae34
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-10-19 22:53:24 -0400

    trying to get the database to work in production

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def get_mongo_collections():
     url = os.environ["MONGO_URL"]
-    client = AsyncIOMotorClient(url, tls=True, tlsCAFile=certifi.where())
+    client = AsyncIOMotorClient(url, tls=True, tlsCAFile=certifi.where(), tlsDisableOCSPEndpointCheck=True)
     db = client["WorkPhone"]
 
     return db["call_logs"], db["docs"], db["user_info"], client
